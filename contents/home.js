cc72aadfab664c02bcd3635f6bcf129be122eaf5
Lex Blagus <lex@x128.com.br>
2016-12-20 14:05:17 -0200

    Working with different target containers

--- contents/home.js
+++ contents/home.js
@@ -13,7 +13,7 @@ self.y256.home = {
 	// ————————————————————————————————————————————————————————————————————————————————
 	init : function(){
 		l.info('y256.home.init' , arguments);
-		if( !y256.model.dataset ){
+		if( !y256.model || !y256.model.dataset ){
 			$.ajax(
 				{
 					'url': y256.options.api.dataset,
@@ -27,6 +27,7 @@ self.y256.home = {
 	loadedDataset : function(response, status, jqXHR){
 		l.info('y256.home.loadedDataset' , arguments);
 		//l.log("JSON.stringify(response,null,'	')", JSON.stringify(response,null,'	'));
+		if( !y256.model ){ y256.model={}; }
 		y256.model.dataset = response;
 		//...
 	},
